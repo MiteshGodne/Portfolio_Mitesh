949c2f9ce1a573b6d32845af55faad9ae26c676f
Mitesh Godne <129673121+MiteshGodne@users.noreply.github.com>
2024-06-09 02:11:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// JS code for Project Slider
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n){
+    let i;
+    let slides = document.getElementsByClassName('project');
+    if( n > slides.length){
+        slideIndex = 1;
+    }
+    if( n < 1){
+        slideIndex = slides.length;
+    }
+    for( i=0; i<slides.length; i++){
+        slides[i].style.display="none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
+
